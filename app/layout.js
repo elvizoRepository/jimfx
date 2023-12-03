04fcdc7eaba834f00cf5935219fb4f8b782a9431
elvizoRepository <152646130+elvizoRepository@users.noreply.github.com>
2023-12-03 08:32:08 +0100

    new

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,17 @@
 import './globals.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+
+export const metadata = {
+  title: 'JIM FX CAPITAL',
+  description: 'Our mission is to deliver superior investment returns while maintaining the highest standards of integrity and transparency',
+}
+
 export default function Layout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>Next.js</title>
+        <title>Jim Fx Capital</title>
       </head>
       <body className='bg_color '>
         <Navbar/>
